07fe7927db37cb1a51eaf8f44183da544afba6b4
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2020-11-23 10:40:33 -0300

    button onclick

--- script.js
+++ script.js
@@ -3,9 +3,20 @@ function perCent(value, total, dia) {
   pC += value / total * 100;
   document.getElementById(week[dia - 2]).innerHTML = `${Math.round(pC)}%`
 }
+function buttonOnClick(button) {
+  button.onclick = function aaa() {
+    document.querySelector('td:nth-child(3) button').innerHTML = 'Hello!';
+    perCent(1,5,4);
+  };
+}
 
 let pC;
 let week = ['tdSeg','tdTer','tdQua','tdQui','tdSex'];
+let buttonList = document.querySelectorAll('button');
+
+for (let i = 0; i < buttonList.length; i++) {
+  buttonOnClick(buttonList[i]);
+}
 
 // document.getElementsByTagName();
 // document.addEventListener('click', );
