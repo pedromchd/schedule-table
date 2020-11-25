f799f2aee077efc361b89e2457d85c30e62e0f10
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2020-11-25 02:33:43 -0300

    color buttons

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function buttonOnClick(button, dia) {
     }
     pC += value;
     document.getElementById(week[dia]).innerHTML = `${pC}%`;
-    button.style.backgroundColor = 'hsl(252, 30%, 67%)';
+    button.style.filter = 'contrast(60%) brightness(120%)';
     button.disabled = true;
   };
 }
@@ -50,6 +50,10 @@ for (let i = 3; i <= 20; i++) {
   if (i%2 == 0) {
     document.querySelector(`tr:nth-child(${i})`).style.backgroundColor = "#f1efed";
   } else {
+    let verticalButton = document.querySelectorAll(`td:nth-child(${i}) button`);
+    for (let j = 0; j < verticalButton.length; j++) {
+      verticalButton[j].style.backgroundColor = '#87549C';
+    }
     document.querySelector(`tr:nth-child(${i})`).style.backgroundColor = "#e9e7f1";
   }
 }
